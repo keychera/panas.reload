fa1156ffc4b1fc12193d9bcdd638fc038f19e2d5
keychera <kyogatama@gmail.com>
2023-04-02 06:34:37 +0700

    test-deps and first unit-test

--- test/unit_test.clj
+++ test/unit_test.clj
@@ -1,20 +1,35 @@
 (ns unit-test
   (:require [clojure.java.io :as io]
             [clojure.test :refer [deftest is testing]]
+            [hickory.core :refer [parse as-hickory]]
+            [hickory.select :as s]
             [panas.reload :refer [with-akar]]))
 
 (defn get-html [res-path]
   (io/file (io/resource (str "htmls/" res-path))))
 
-(declare htmx-installed?)
+(defn have?
+  "true if coll have elm"
+  [coll elm]
+  (some #(= elm %) coll))
+
+(defn htmx-installed? [{:keys [body]}]
+  (let [script-srcs (->> (parse body) as-hickory
+                         (s/select (s/tag :head)) first :content
+                         (filter #(= (:tag %) :script)) (map :attrs) (map :src)
+                         (into #{}))]
+    (is (have? script-srcs "https://unpkg.com/htmx.org@1.8.4"))
+    (is (have? script-srcs "https://unpkg.com/htmx.org@1.8.4/dist/ext/ws.js"))))
+
 (deftest test!with-akar
   (testing "Testing with-akar for"
     (let [normal-html-file (get-html "01_normal.html")]
       (testing "normal html"
-        (is (htmx-installed? (with-akar {:body normal-html-file})) "with File :body")
-        (is (htmx-installed? (with-akar {:body (slurp normal-html-file)})) "with String :body")))))
+        (testing "with File :body"
+          (htmx-installed? (with-akar {:body normal-html-file})))
+        (testing "with String :body"
+          (htmx-installed? (with-akar {:body (slurp normal-html-file)})))))))
 
-(defn htmx-installed? [{:keys [body]}] false)
 
 ;; String
 

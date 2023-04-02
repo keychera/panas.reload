db13f6d7543f3e4a90767b8a12fcd47fbb95f113
keychera <kyogatama@gmail.com>
2023-04-02 09:04:01 +0700

    attempt to test 4 case, not yet a good code

--- test/unit_test.clj
+++ test/unit_test.clj
@@ -8,18 +8,18 @@
 (defn get-html [res-path]
   (io/file (io/resource (str "htmls/" res-path))))
 
-(defn have?
-  "true if coll have elm"
-  [coll elm]
-  (some #(= elm %) coll))
+(defn should-have [count item freq]
+  (= (get freq item) count))
 
-(defn htmx-installed? [{:keys [body]}]
-  (let [script-srcs (->> (parse body) as-hickory
-                         (s/select (s/tag :head)) first :content
-                         (filter #(= (:tag %) :script)) (map :attrs) (map :src)
-                         (into #{}))]
-    (is (have? script-srcs "https://unpkg.com/htmx.org@1.8.4"))
-    (is (have? script-srcs "https://unpkg.com/htmx.org@1.8.4/dist/ext/ws.js"))))
+(defn htmx-installed?
+  ([ring-res] (htmx-installed? ring-res nil nil))
+  ([{:keys [body]} htmx-src htmx-ws-src]
+   (let [script-srcs (->> (parse body) as-hickory
+                          (s/select (s/tag :head)) first :content
+                          (filter #(= (:tag %) :script)) (map :attrs) (map :src)
+                          frequencies)]
+     (is (should-have 1 (or htmx-src "https://unpkg.com/htmx.org@1.8.4") script-srcs))
+     (is (should-have 1 (or htmx-ws-src "https://unpkg.com/htmx.org@1.8.4/dist/ext/ws.js") script-srcs)))))
 
 (deftest test!with-akar
   (testing "Testing with-akar for"
@@ -28,7 +28,27 @@
         (testing "with File :body"
           (htmx-installed? (with-akar {:body normal-html-file})))
         (testing "with String :body"
-          (htmx-installed? (with-akar {:body (slurp normal-html-file)})))))))
+          (htmx-installed? (with-akar {:body (slurp normal-html-file)})))))
+    (let [with-htmx-file (get-html "02_with_htmx.html")]
+      (testing "html with htmx"
+        (testing "with File :body"
+          (htmx-installed? (with-akar {:body with-htmx-file})))
+        (testing "with String :body"
+          (htmx-installed? (with-akar {:body (slurp with-htmx-file)})))))
+    (let [with-htmx-ws-file (get-html "03_with_htmx_ws.html")]
+      (testing "html with htmx and ws"
+        (testing "with File :body"
+          (htmx-installed? (with-akar {:body with-htmx-ws-file})))
+        (testing "with String :body"
+          (htmx-installed? (with-akar {:body (slurp with-htmx-ws-file)})))))
+    (let [htmx-src "https://unpkg.com/htmx.org@1.8.6" ;; the same as inside the file
+          htmx-ws-src "https://unpkg.com/htmx.org@1.8.6/dist/ext/ws.js"
+          with-htmx-ws-file (get-html "04_with_newer_htmx.html")]
+      (testing "html with newer htmx"
+        (testing "with File :body"
+          (htmx-installed? (with-akar {:body with-htmx-ws-file}) htmx-src htmx-ws-src))
+        (testing "with String :body"
+          (htmx-installed? (with-akar {:body (slurp with-htmx-ws-file)}) htmx-src htmx-ws-src))))))
 
 
 ;; String

26a29f7a050b612cd57c340673210d7e4824e0d1
keychera <kyogatama@gmail.com>
2023-04-05 08:12:59 +0700

    move test-resources and some negative test

--- test/unit_test.clj
+++ test/unit_test.clj
@@ -6,8 +6,8 @@
             [hickory.select :as s]
             [panas.reload :refer [with-akar]]))
 
-(defn get-html [res-path]
-  (io/file (io/resource (str "htmls/" res-path))))
+(defn get-res [res-path]
+  (io/file (io/resource res-path)))
 
 (defn at [hickory tag] (->> hickory (s/select (s/tag tag)) first))
 
@@ -26,7 +26,7 @@
 
 (deftest with-akar!test-normal-htmx
   (testing "Testing with-akar for normal html"
-    (let [normal-html-file (get-html "01_normal.html")
+    (let [normal-html-file (get-res "01_normal.html")
           normal-html-str (slurp normal-html-file)
           prev-hickory (-> (parse normal-html-str) as-hickory)]
       (testing "with File :body"
@@ -46,7 +46,7 @@
 
 (deftest with-akar!test-existing-htmx
   (testing "Testing with-akar for html with existing htmx"
-    (let [with-htmx-file (get-html "02_with_htmx.html")
+    (let [with-htmx-file (get-res "02_with_htmx.html")
           with-htmx-str (slurp with-htmx-file)
           prev-hickory (-> (parse with-htmx-str) as-hickory)]
       (testing "with File :body"
@@ -68,7 +68,7 @@
 
 (deftest with-akar!test-existing-htmx-ws
   (testing "Testing with-akar for html with existing htmx and ws extension"
-    (let [with-htmx-ws-file (get-html "03_with_htmx_ws.html")
+    (let [with-htmx-ws-file (get-res "03_with_htmx_ws.html")
           with-htmx-ws-str (slurp with-htmx-ws-file)
           prev-hickory (-> (parse with-htmx-ws-str) as-hickory)]
       (testing "with File :body"
@@ -92,7 +92,7 @@
 
 (deftest with-akar!test-no-head
   (testing "Testing with-akar for html with no head"
-    (let [no-head-html (get-html "04_no_head.html")
+    (let [no-head-html (get-res "04_no_head.html")
           no-head-str (slurp no-head-html)]
       (testing "with File :body"
         (let [{body :body} (with-akar {:body no-head-html})]
@@ -102,8 +102,8 @@
           (is (= body no-head-str) "should have no change"))))))
 
 (deftest with-akar!test-no-body
-  (testing "Testing with-akar for html with no head"
-    (let [no-body-html (get-html "05_no_body.html")
+  (testing "Testing with-akar for html with no body"
+    (let [no-body-html (get-res "05_no_body.html")
           no-body-str (slurp no-body-html)]
       (testing "with File :body"
         (let [{body :body} (with-akar {:body no-body-html})]
@@ -113,8 +113,8 @@
           (is (= body no-body-str) "should have no change"))))))
 
 (deftest with-akar!test-no-html-tag
-  (testing "Testing with-akar for html with no head"
-    (let [no-htmltag-html (get-html "06_no_html_tag.html")
+  (testing "Testing with-akar for html with html tag"
+    (let [no-htmltag-html (get-res "06_no_html_tag.html")
           no-htmltag-str (slurp no-htmltag-html)]
       (testing "with File :body"
         (let [{body :body} (with-akar {:body no-htmltag-html})]
@@ -123,10 +123,21 @@
         (let [{body :body} (with-akar {:body no-htmltag-str})]
           (is (= body no-htmltag-str) "should have no change"))))))
 
-
-(comment
-  (with-akar {:body (get-html "03_with_htmx_ws.html")})
-  (with-akar {:body (get-html "04_no_head.html")}))
-
-;; not html at all (.css .svg .png)
-;; not File or string (InputStream, ISeq)
+(deftest with-akar!test-not-html-at-all
+  (testing "Testing with-akar for css body"
+    (let [css-body (get-res "mock/style.css")
+          {body :body} (with-akar {:body css-body})]
+      (is (= body css-body) "should have no change")))
+  (testing "Testing with-akar for svg body"
+    (let [svg-body (get-res "mock/some.svg")
+          {body :body} (with-akar {:body svg-body})]
+      (is (= body svg-body) "should have no change")))
+  ;; these two below might change
+  (testing "Testing with-akar for Input stream"
+    (let [input-stream-body (io/input-stream (io/resource "01_normal.html"))
+          {body :body} (with-akar {:body input-stream-body})]
+      (is (= body input-stream-body) "should have no change")))
+  (testing "Testing with-akar for ISeq"
+    (let [iseq-body ["this" "is" "hello!"]
+          {body :body} (with-akar {:body iseq-body})]
+      (is (= body iseq-body) "should have no change"))))
